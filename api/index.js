990a81a3ddfd07db527e0ec4b04da0640e3404a3
Olamilekan <olamilekanopeyemi0308@gmail.com>
2026-09-01 19:29:38 +0100

    fix: expose Express app through Vercel handler

--- api/index.js
+++ api/index.js
@@ -1,3 +1,9 @@
 import app from "../api-server/src/app.ts";
 
-export default app;
+export const config = {
+  maxDuration: 30,
+};
+
+export default function handler(req, res) {
+  return app(req, res);
+}
