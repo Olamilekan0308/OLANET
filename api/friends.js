33fe7e3216f47e029600d2948607549c7198b417
Olamilekan <olamilekanopeyemi0308@gmail.com>
2026-09-02 07:48:30 +0100

    fix: make friends wrapper reach social handler

--- api/friends.js
+++ api/friends.js
@@ -3,6 +3,6 @@ import handler from "./social.js";
 export default function friends(req, res) {
   const url = new URL(req.url, "http://localhost");
   url.searchParams.set("route", "friends");
-  req.url = `${url.pathname}?${url.searchParams.toString()}`;
+  req.url = `/?${url.searchParams.toString()}`;
   return handler(req, res);
 }
