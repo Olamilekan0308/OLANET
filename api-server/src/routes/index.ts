fd5b158a6d067edce3ee262b3b0f0359892982d0
Olamilekan <olamilekanopeyemi0308@gmail.com>
2026-09-05 01:05:41 +0100

    fix: mount frontend-compatible messages API

--- api-server/src/routes/index.ts
+++ api-server/src/routes/index.ts
@@ -5,6 +5,7 @@ import olanetAIRouter from "./olanet-ai-v2";
 import supportAIRouter from "./support-ai";
 import circlesRouter from "./circles";
 import socialRouter from "./social";
+import messagesRouter from "./messages";
 import settingsRouter from "./settings";
 import friendsRouter from "./friends";
 
@@ -16,6 +17,7 @@ router.use(olanetAIRouter);
 router.use(supportAIRouter);
 router.use(circlesRouter);
 router.use(socialRouter);
+router.use("/messages", messagesRouter);
 router.use(settingsRouter);
 router.use(friendsRouter);
 
