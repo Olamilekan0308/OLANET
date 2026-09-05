78e6a34d91596fd452782746ec046683acfabe92
Olamilekan <olamilekanopeyemi0308@gmail.com>
2026-09-05 01:22:50 +0100

    feat: route settings hub

--- skillhub/src/main.tsx
+++ skillhub/src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import App from './App';
 import DepartmentHub from '@/components/department-hub';
 import PeopleHub from '@/components/people-hub';
+import SettingsHub from '@/components/settings-hub';
 import { ErrorBoundary } from '@/components/error-boundary';
 
 import './index.css';
@@ -11,6 +12,7 @@ function RootApp() {
   const path = window.location.pathname;
   if (path === '/circles' || /^\/circles\/\d+$/.test(path)) return <DepartmentHub />;
   if (path === '/people' || path === '/friends' || path === '/search') return <PeopleHub />;
+  if (path === '/settings' || path === '/settings/account') return <SettingsHub />;
   return <App />;
 }
 
